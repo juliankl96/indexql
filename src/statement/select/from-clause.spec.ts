0f9468bf8596ea6d2f180db1b6c7284175a77bbd
jklaemke <j.klaemke@icloud.com>
2024-05-26 15:52:54 +0200

    feat: add from-clause.ts test

--- src/statement/select/from-clause.spec.ts
+++ src/statement/select/from-clause.spec.ts
@@ -6,6 +6,21 @@ describe('FromClause', () => {
         const fromClause = new FromClause('person p', 'address a');
         const aliases = fromClause.aliases;
         expect(aliases.length).toBe(2);
+
+        expect(aliases[0].alias).toBe("p")
+        expect(aliases[0].table).toBe("person")
+
+        expect(aliases[1].alias).toBe("a")
+        expect(aliases[1].table).toBe("address")
+    });
+
+    it('should handle one table', () => {
+        const fromClause = new FromClause('person');
+        const aliases = fromClause.aliases;
+        expect(aliases.length).toBe(1);
+
+        expect(aliases[0].table).toBe("person")
+        expect(aliases[0].alias).toBeUndefined();
     });
 
 });
