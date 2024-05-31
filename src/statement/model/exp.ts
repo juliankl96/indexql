59324c648395c016ec79e8d2a136ae4a2c4a3371
jklaemke <j.klaemke@icloud.com>
2024-05-31 18:02:11 +0200

    feat: create models from documentation

--- src/statement/model/exp.ts
+++ src/statement/model/exp.ts
@@ -1,4 +1,6 @@
 import {TYPE} from "./data-types";
+import {UnaryOperator} from "./unnary-operation";
+import {BitwiseOperation} from "./bitwise-operation";
 
 export interface Exp {
 
@@ -27,5 +29,76 @@ export class BindParameter implements Exp {
 
     get parameter(): string {
         return this._parameter;
+    }
+}
+
+export class Column implements Exp {
+
+    private _schema?: string;
+    private _table?: string;
+    private _column: string;
+
+
+    constructor(param: string) {
+        const regex = /((\w+)\.)?((\w+)\.)?(\w+)/gmi;
+        const matches = regex.exec(param);
+        if (matches === null) {
+            throw new Error('Invalid column');
+        }
+        this._schema = matches[2];
+        this._table = matches[4];
+        this._column = matches[5];
+    }
+
+    get table(): string {
+        return this._table;
+    }
+
+    get column(): string {
+        return this._column;
+    }
+}
+
+export class UnaryOperation implements Exp {
+
+    private _operator: UnaryOperator;
+    private _exp: Exp;
+
+    constructor(operator: UnaryOperator, exp: Exp) {
+        this._operator = operator;
+        this._exp = exp;
+    }
+
+    get operator(): string {
+        return this._operator;
+    }
+
+    get exp(): Exp {
+        return this._exp;
+    }
+}
+
+export class BinaryOperation implements Exp {
+
+    private _left: Exp;
+    private _right: Exp;
+    private _operator: BitwiseOperation
+
+    constructor(left: Exp, operator: BitwiseOperation, right: Exp) {
+        this._left = left;
+        this._right = right;
+        this._operator = operator;
+    }
+
+    get left(): Exp {
+        return this._left;
+    }
+
+    get right(): Exp {
+        return this._right;
+    }
+
+    get operator(): BitwiseOperation{
+        return this._operator;
     }
 }
---
+++ src/statement/model/unnary-operation.ts
@@ -0,0 +1,9 @@
+export declare type NOT = 'NOT';
+
+export declare type MINUS = '-';
+
+export declare type PLUS = '+';
+
+export declare type BITWISE_NOT = '~';
+
+export declare type UnaryOperator = NOT | MINUS | PLUS | BITWISE_NOT;
